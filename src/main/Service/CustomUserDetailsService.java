a69c540edd5fb22609d5270c51bca83c892d0b8e
JosephAnbarasu <131425907+JosephAnbarasu@users.noreply.github.com>
2023-12-17 14:19:42 +0530

    Add files via upload

---
+++ src/main/Service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.employee.management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.employee.management.config.CustomUserDetails;
+import com.employee.management.model.User;
+import com.employee.management.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService  {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+        if(user ==null) {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        return new CustomUserDetails(user);
+    }
+}
